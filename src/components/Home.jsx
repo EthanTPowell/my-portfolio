3819517390ffa50b125009d713da7c6d95370028
EthanTPowell <ethantpowell00@gmail.com>
2022-11-05 20:15:36 -0400

    some routing

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+      <div>Home
+          <h1>Welcome to my portfolio rough draft. We've just now started React so I don't have much to show for it yet.</h1>
+
+    </div>
+  )
+}
